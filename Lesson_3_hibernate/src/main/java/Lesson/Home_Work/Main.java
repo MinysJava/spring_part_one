5be546810a6ee057493abc427712ac31b9a92193
shein dima <msh86.02.18@rambler.ru>
2020-04-12 16:49:20 +0500

    ДЗ 3 Создал кдассы добавил маппинг в конфиг

---
+++ Lesson_3_hibernate/src/main/java/Lesson/Home_Work/Main.java
@@ -0,0 +1,7 @@
+package Lesson.Home_Work;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Lesson_3_hibernate/src/main/java/Lesson/Home_Work/Product.java
@@ -0,0 +1,51 @@
+package Lesson.Home_Work;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String cost;
+
+    public Product() {
+    }
+
+    public Product(String title, String cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+}
