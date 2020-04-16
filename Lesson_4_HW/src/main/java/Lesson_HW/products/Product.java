bc1fd107a1e0f79af0c74f0cd2035311fd688922
shein dima <msh86.02.18@rambler.ru>
2020-04-16 13:50:02 +0500

    Урок 4 Сделал классы

---
+++ Lesson_4_HW/src/main/java/Lesson_HW/products/Product.java
@@ -0,0 +1,51 @@
+package Lesson_HW.products;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private BigDecimal cost;
+
+    public Product() {
+    }
+
+    public Product(String title, BigDecimal cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+}
---
+++ Lesson_4_HW/src/main/java/Lesson_HW/service/ProductService.java
@@ -0,0 +1,42 @@
+package Lesson_HW.service;
+
+import Lesson_HW.products.Product;
+import Lesson_HW.products.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public void insert(Product product){
+        productRepository.save(product);
+    }
+
+    @Transactional
+    public void update(Product product){
+        productRepository.save(product);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> getAllPersons(){
+        return productRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(Long id){
+        return productRepository.findById(id);
+    }
+}
