8a589c68078ea6e7d1ae0167c1fe4cc063f97eeb
shein dima <msh86.02.18@rambler.ru>
2020-04-25 19:32:43 +0500

    Урок 7 Обработка исключений

---
+++ Lesson_6/src/main/java/Lesson/rest/NotFoundException.java
@@ -0,0 +1,4 @@
+package Lesson.rest;
+
+public class NotFoundException extends RuntimeException{
+}
--- Lesson_6/src/main/java/Lesson/rest/ProductResource.java
+++ Lesson_6/src/main/java/Lesson/rest/ProductResource.java
@@ -3,6 +3,8 @@ package Lesson.rest;
 import Lesson.product.Product;
 import Lesson.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,11 +27,14 @@ public class ProductResource {
 
     @GetMapping(path = "/{id}/id", produces = "application/json")
     public Product findById(@PathVariable("id") Long id){
-        return productService.findById(id).get();
+        return productService.findById(id).orElseThrow(NotFoundException::new);
     }
 
     @PostMapping
     public void createProduct(@RequestBody Product product){
+        if (product.getId() != null){
+            throw new IllegalArgumentException("что-нибудь!");
+        }
         productService.save(product);
     }
 
@@ -39,7 +44,17 @@ public class ProductResource {
     }
 
     @DeleteMapping(path = "/{id}/id", produces = "application/json")
-    public void deleteProduct(Long id){
+    public void deleteProduct(@PathVariable("id") Long id){
         productService.deleteById(id);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> notFoundExceptionHandler(NotFoundException exception){
+        return new ResponseEntity<>("NOT_FOUND", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> illegalArgumentException(IllegalArgumentException exception){
+        return new ResponseEntity<>(exception.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
     }
 }
